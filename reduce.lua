4cca3138dcb671d1b54c6d0aaec3913327074919
Heiko Goes <heiko.goes@gaebler-goes.de>
2025-11-11 10:24:20 +0100

    fixed call to add-function in reduce -> must be f-function

--- reduce.lua
+++ reduce.lua
@@ -1,16 +1,15 @@
-function add(previous, next)
-  return previous + next
-end
-
 function reduce(max, init, f)
   local acc = init
 
   for i = init + 1, max do
-    acc = add(acc, i)
+    acc = f(acc, i)
   end
 
   return acc
 end
 
+function add(previous, next)
+  return previous + next
+end
 
 print("reduce(10, 1, add)", reduce(10, 1, add))
